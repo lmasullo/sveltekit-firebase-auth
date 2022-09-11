bd2846957e6c9049c2a9fb33e923065f8474f468
Lawrence N. Masullo <lmasullo@gmail.com>
2022-09-11 15:47:33 -0500

    apiKey

--- src/lib/fb.js
+++ src/lib/fb.js
@@ -7,7 +7,7 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyDuQ1P7uoqdXmmmEuKqxQwVmVexNnLcv3Q',
+	apiKey: '1234567890',
 	authDomain: 'mrpro-maz.firebaseapp.com',
 	databaseURL: 'https://mrpro-maz.firebaseio.com',
 	projectId: 'mrpro-maz',
